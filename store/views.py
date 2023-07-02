8ecb95cbb47b32a2227680df133f7a1839590480
morelucks <luckykamshak@gmail.com>
2023-07-02 22:02:13 +0000

    search functionality

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from django.db.models import Q
 
 
 def store(request, category_slug=None):
@@ -42,3 +42,15 @@ def product_detail(request, category_slug, product_slug):
         'in_cart':in_cart
     }
    return render(request, 'store/product_detail.html', context)  
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        if keyword:
+            products=Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword)|Q(product_name=keyword))
+            product_count=products.count()
+        context={
+            'products':products,
+            'product_count':product_count
+        }
+    return render(request, 'store/store.html', context  )
