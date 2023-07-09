7356e716b14540a2ee7ab62af2c3419bcb231c86
morelucks <luckykamshak@gmail.com>
2023-07-09 22:23:14 +0000

    reg tempt

--- accounts/form.py
+++ accounts/form.py
@@ -1,6 +1,23 @@
 from django import forms
 from .models import Account
 class RegistrationForm(forms.ModelForm):
+    password=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password'
+    }))
+    confirm_password=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm Password'
+    }))
     class Meta:
         model=Account
         fields=['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+# css class function
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder']='Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder']='Enter last Name'
+        self.fields['phone_number'].widget.attrs['placeholder']='Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder']='Enter Email Adress'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
