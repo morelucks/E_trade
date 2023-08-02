aa0627a7b27c1acfdd99a8ceb9ae7c20b0a8160e
morelucks <luckykamshak@gmail.com>
2023-08-02 22:11:33 +0000

    allset

--- E_trade/settings.py
+++ E_trade/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATABASE_URL="postgresql://postgres:8Eksr7AMixKLeTaRg2tR@containers-us-west-207.railway.app:6689/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -82,10 +82,7 @@ AUTH_USER_MODEL='accounts.Account'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
